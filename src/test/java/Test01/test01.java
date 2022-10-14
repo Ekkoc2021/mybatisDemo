6745a872cbe7e7e2c4a2f2f4361604f2197caffb
ekko <113243745+Ekkoc2021@users.noreply.github.com>
2022-10-14 20:34:38 +0800

    Update test01.java

--- src/test/java/Test01/test01.java
+++ src/test/java/Test01/test01.java
@@ -18,7 +18,7 @@ import java.util.List;
 
 public class test01 {
     public static SqlSession getSession(){
-        //读取MyBatis的核心配置文件
+        //读取核心配置文件
         InputStream is = null;
         try {
             is = Resources.getResourceAsStream("mybatis.xml");
@@ -28,11 +28,9 @@ public class test01 {
         //创建SqlSessionFactoryBuilder对象
         SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new
                 SqlSessionFactoryBuilder();
-        //通过核心配置文件所对应的字节输入流创建工厂类SqlSessionFactory，生产SqlSession对象
+        //通过核心配置文件所对应的字节输入流获取SqlSessionFactory
         SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(is);
-        //创建SqlSession对象，此时通过SqlSession对象所操作的sql都必须手动提交或回滚事务
-        //SqlSession sqlSession = sqlSessionFactory.openSession();
-        //创建SqlSession对象，此时通过SqlSession对象所操作的sql都会自动提交
+        //设置自动提交
         SqlSession sqlSession = sqlSessionFactory.openSession(true);
         return sqlSession;
     }
